30c57c4a364580a7217d5db43034bf8904f815cd
Ian C <ian@ik2.us>
2015-01-25 22:50:41 -0800

    Update function.php

--- function.php
+++ function.php
@@ -1,13 +1,14 @@
+/* Custom functions */
 function add_my_post_types_to_query( $query ) {
   if ( is_home() && $query->is_main_query() )
     $query->set( 'post_type', array( 'post', 'recipe' ) );
   return $query;
 }
 function custom_excerpt_length( $length ) {
-	return 20;
+  return 20;
 }
 add_filter( 'excerpt_length', 'custom_excerpt_length', 999 );
 function new_excerpt_more( $more ) {
-	return ' <a class="read-more" href="'. get_permalink( get_the_ID() ) . '">' . __('Read More', 'your-text-domain') . '</a>';
+  return '... <br/><a class="read-more" href="'. get_permalink( get_the_ID() ) . '"><em>' . __('Read More', 'your-text-domain') . '</em></a>';
 }
 add_filter( 'excerpt_more', 'new_excerpt_more' );
